1c6a4929ad548b0a5586edb470064547d8859262
MohitSaini <mailtomohit2002@gmail.com>
2022-05-28 22:50:56 +0530

    JWT Authentication + password hashing using bcryptjs

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const User = require('../models/User.js');       // this Schema is to save the user data
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET='mohitsainiislearningreact';
 
 // Create a user using :POST "api/auth/createuser" --> new user creation
 
@@ -50,13 +54,30 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ errors: "sorry user with the same email already exists" });
         }
+
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email
-        })
+        });
 
-        res.json({ "result": "successful" })
+
+        // ! JWT is simply an authentication way used to ensure a secure environment for data transfer between user and client.
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        //JWT_SECRET will help us in knowing whether any user has tried to temper with  it or not.
+        const authtoken=jwt.sign(data, JWT_SECRET);
+        
+        // whenever anyone would provide us with authtoken we can regain user.id from it
+
+        res.json({authtoken})
     } catch (error) {
 
         console.log(error);
