2695393e6a14ce46b40f97c1683dcdfa50013cb4
MohitSaini <mailtomohit2002@gmail.com>
2022-05-30 23:38:54 +0530

    harry sir's method for note creation could also run well

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -6,46 +6,50 @@ const Notes = require('../models/Notes');
 
 // Route 1: Get all the notes using /api/notes/fetchallnotes route
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
-
     // our middleware fetchuser has put req.user=data.user
-    const notes = await Notes.find({ user: req.user.id })
-    res.json(notes)
+    try {
+
+        // this will give all the notes corresponding tothe user.id
+        const notes = await Notes.find({ user: req.user.id })
+        res.json(notes)
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error")
+    }
+
 })
 
 
 // Route 1: Creating the user notes using /api/notes/createnotes routes
 
-router.post('/createnotes', fetchuser, [
+router.post('/addnote', fetchuser, [
     // !validation of input data is here
     body('title', 'Enter a valid title').isLength({ min: 3 }),
-    body('tag', 'entry a tag').exists(),
     body('description', 'entry a tag').isLength({ min: 5 })
 ], async (req, res) => {
-    // let user = await User.findOne({ email: req.body.email });
 
-    //---->>>>>>>>>>>>>>>>>>>>>>>>>>> User is the collection name out here
-    // user = await User.create({
-    //     name: req.body.name,
-    //     password: secPass,
-    //     email: req.body.email
-    // });
-    // i am getting req.user=data.user
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        /* this will come in if there are some errors regarding our user inputs.*/
-        return res.status(400).json({ errors: errors.array() });
-    }
 
     try {
-        let note = await Notes.create({
-            user: req.user.id,
-            title: req.body.title,
-            description: req.body.title,
-            tag: req.body.tag
+
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            /* this will come in if there are some errors regarding our user inputs.*/
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // this is what we call as destructuring of js element
+        const { title, description, tag } = req.body;
+        const note = new Notes({
+            title, description, tag, user: req.user.id
         })
-        console.log(req.body.title)
-        res.json(req.body)
+
+        const savedNote = await note.save();
+
+        res.json(savedNote);
+
     } catch (error) {
         console.log(error);
         res.status(500).send("Internal server error")
